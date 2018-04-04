1b2d8870150f21b2a0bcea150c83be29fc26ef50
Berina Omerasevic <bomerasevi1@etf.unsa.ba>
2018-04-04 14:13:11 +0200

    Dijagram Klasa

--- Projekat/eCopy/ECopy/Registracija_firma.xaml.cs
+++ Projekat/eCopy/ECopy/Registracija_firma.xaml.cs
@@ -13,34 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace ECopy
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Registracija_firma : Page
     {
-        public MainPage()
+        public Registracija_firma()
         {
             this.InitializeComponent();
-        }
-
-        private void MessageTextBlock_SelectionChanged(System.Object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Prijava_Click(object sender, RoutedEventArgs e)
-        {
-
-            this.Frame.Navigate(typeof(LogIn));
-        }
-
-        private void Registracija_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(Registracija_f));
         }
     }
 }
